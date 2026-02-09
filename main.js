7baa9e2c64ec84509ff895b673aa5d83cad0321d
jana13 <janatamer583@gmail.com>
2026-02-09 18:39:19 +0200

    primo

---
+++ main.js
@@ -0,0 +1,37 @@
+const initialUsers = [
+    {
+        id: 1,
+        name: "Omar Ahmed",
+        email: "Omar.Ahemd@gmail.com",
+        age: 21,
+        specialty: "Backend Developer"
+    },
+    {
+        id: 2,
+        name: "Jana Tamer",
+        email: "janaTamer583@gmail.com",
+        age: 20,
+        specialty: "Frontend Developer"
+    },
+    {
+        id: 3,
+        name: "Gumball Watterson",
+        email: "gumball@gmail.com",
+        age: 26,
+        specialty: "Manager"
+    },
+    {
+        id: 4,
+        name: "Eren Yeger",
+        email: "Rumbling@gmail.com",
+        age: 19,
+        specialty: "Pentration Tester"
+    },
+    {
+        id: 5,
+        name: "Levi Ackerman",
+        email: "Levi@gmail.com",
+        age: 23,
+        specialty: "Data Analyst"
+    }
+];
